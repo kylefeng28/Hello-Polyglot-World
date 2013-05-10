b47f247479f77388a69cb26ef7e7f2aba4215c49
Caleb Xu <calebcenter@live.com>
2013-05-10 14:07:24 -0400

    Added languages: Obix, PowerShell, Rust, Scala, Scheme, SQL, Tcl, Vala

---
+++ sql/hello.sql
@@ -0,0 +1,3 @@
+SELECT 'Hello, world!' FROM DUMMY; -- DUMMY is a standard table in SAP HANA.
+SELECT 'Hello, world!' FROM DUAL; -- DUAL is a standard table in Oracle.
+SELECT 'Hello, world!' -- This will work in SQL Server.
