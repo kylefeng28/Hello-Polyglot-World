7287fd301b0329442d76ea60d93a58617ae0bf07
Kyle Feng <AeroFengBlade@gmail.com>
2013-05-17 16:18:42 -0400

    Organized file structure
    Renamed all source code to hello.*
    Chmod'ed assembly binaries

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Hello-World
 ===========
 
 Hello World in many languages (programming and human)
+
+#Todo
+Add more languages
--- c++/hello.cpp
+++ c++/hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
- 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(int argc, char *argv[], char *envp[]) {
+    cout << "Hello, world!" << endl;
     return 0;
 }
